be04699118e0349492a1dda87e2cda90fc9dd05b
zhangjincheng <382462237@qq.com>
2020-04-11 23:41:54 +0800

    Spring的第一节

---
+++ 1.SpringIOC_01/src/bean/Person.java
@@ -0,0 +1,38 @@
+package bean;
+
+public class Person {
+	private String name;
+	private int age;
+	private String gender;
+	private String email;
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", gender=" + gender + ", email=" + email + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
